e75adb8ec2686a724c4a012675dc217e783daa4e
Robert Dudzik <Zarathulpl0x@yahoo.com>
2019-03-07 09:29:24 -0500

    Revert "Fixed Conflicts"
    This reverts commit ffa9c4218e3e0141fb9dd16ecd7203c31125c712.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+<<<<<<< HEAD
+import { SelectionComponent } from './components/selection/selection.component';
+import { EatoutComponent } from './components/eatout/eatout.component';
+import { EatinComponent } from './components/eatin/eatin.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+=======
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginRegModalComponent } from './components/login-reg-modal/login-reg-modal.component';
+>>>>>>> Edwin
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+<<<<<<< HEAD
+    SelectionComponent,
+    EatoutComponent,
+    EatinComponent,
+    NavbarComponent
+=======
     NavbarComponent,
     LoginRegModalComponent
+>>>>>>> Edwin
   ],
   imports: [
     BrowserModule,
