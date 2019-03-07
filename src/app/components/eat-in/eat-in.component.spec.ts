a7a98d3aea23193699238a6844fdc7204bcf3410
Sanford Zheng <sanfordzheng@gmail.com>
2019-03-07 12:52:18 -0500

    my branch is best branch for real now

--- src/app/components/eat-in/eat-in.component.spec.ts
+++ src/app/components/eat-in/eat-in.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EatInComponent } from './eat-in.component';
 
-describe('EatinComponent', () => {
+describe('EatInComponent', () => {
   let component: EatInComponent;
   let fixture: ComponentFixture<EatInComponent>;
 
