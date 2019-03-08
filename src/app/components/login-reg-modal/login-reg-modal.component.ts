d8c5595063e84c4afce9133e15e8e349870bbfe4
Edwin Faican <edwinxfaican@gmail.com>
2019-03-07 19:53:06 -0500

    Resolved an issue with register modal

--- src/app/components/login-reg-modal/login-reg-modal.component.ts
+++ src/app/components/login-reg-modal/login-reg-modal.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Subscription} from 'rxjs';
 import {ModalService} from '../../services/modal/modal.service'
 import { User } from '../../models/user.model'
-import { Agent } from 'https';
+import { LoginService } from '../../services/login/login.service'
 
 @Component({
   selector: 'app-login-reg-modal',
@@ -15,21 +15,21 @@ export class LoginRegModalComponent implements OnInit {
   subscription:Subscription;
   un: string;
   pw: string;
-  // hi: number;
-  // we: number;
-  // age: number;
-  // sex: string;
+  hi: number;
+  we: number;
+  age: number;
+  sex: string;
 
-  // user: User = {
-  //   username: this.un,
-  //   password: this.pw,
-  //   height: this.hi,
-  //   weight: this.we,
-  //   age: this.age,
-  //   sex: this.sex
-  // };
+  user: User = {
+    username: this.un,
+    password: this.pw,
+    height: this.hi,
+    weight: this.we,
+    age: this.age,
+    sex: this.sex
+  };
 
-  constructor(private _navService: ModalService) { }
+  constructor(private _navService: ModalService, private loginService: LoginService) { }
 
   ngOnInit() {
     this.subscription = this._navService.showItem$.subscribe(showModal => this.display = showModal);
@@ -48,6 +48,25 @@ export class LoginRegModalComponent implements OnInit {
   }
 
   logUser() {
-
+    this.loginService.login(this.user).subscribe(
+      resp => {
+        if(resp != null) {
+          console.log("We have succeeded.");
+        }
+      }
+    );
   }
+
+  regUser() {
+    this.loginService.login(this.user).subscribe(
+      resp => {
+        if(resp != null) {
+          console.log("We have succeeded.");
+        }
+      }
+    );
+  }
+
+
+
 }
