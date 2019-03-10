9716fe5e0e91558ddea5b02b5c650e546ac63872
Sanford <sanfordzheng@gmail.com>
2019-03-10 14:10:17 -0400

    eat-in and router modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { TestApiComponent } from './components/test-api/test-api.component';
 import { PreferencesProfileComponent } from './components/preferences-profile/preferences-profile.component';
+import { EatInComponent } from './components/eat-in/eat-in.component';
+import { EatOutComponent } from './components/eat-out/eat-out.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'test', component: TestApiComponent},
   {path: 'profile', component: PreferencesProfileComponent},
+  {path: 'eat-in', component: EatInComponent},
+  {path: 'eat-out', component: EatOutComponent},
   
+  // NOT PART OF THE APP
+  {path: 'test', component: TestApiComponent},
 ];
 
 @NgModule({
