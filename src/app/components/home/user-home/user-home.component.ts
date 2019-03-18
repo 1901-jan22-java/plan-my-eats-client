85cf1dd2c7afbea7e1ef2a72f47e2d679c6ae2db
Sanford <sanfordzheng@gmail.com>
2019-03-18 02:24:54 -0400

    displaying user preferences need to get pagination working

--- src/app/components/home/user-home/user-home.component.ts
+++ src/app/components/home/user-home/user-home.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user/user.service';
+import { Preference } from 'src/app/models/preference.model';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-user-home',
@@ -11,13 +13,22 @@ import { UserService } from 'src/app/services/user/user.service';
 export class UserHomeComponent implements OnInit {
 
   user: User = new User();
+  tableColumns: string[] = [
+    'prefs',
+  ];
+  dataSource: MatTableDataSource<Preference> = new MatTableDataSource<Preference>(this.user.preferences);
+
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
 
   constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
     this.userService.user$.subscribe(resp => {
       console.log(resp);
       this.user = resp;
+      this.dataSource = new MatTableDataSource<Preference>(resp.preferences);
     });
   }
 
